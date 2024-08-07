fccc31a6751c09e1627d84a58f8299c821e8a41b
urpadey <profiletesting121@gmail.com>
2024-08-07 13:18:35 +0100

    update

--- client/src/App.js
+++ client/src/App.js
@@ -200,7 +200,7 @@ const Suppliers = lazy(() => {
   return import("./container/StoreServices/Suppliers/Suppliers");
 });
 
-const ENDPOINT = "http://localhost:3001";
+const ENDPOINT = "http://192.168.88.3:3001";
 const socket = io(ENDPOINT, {
   transports: ["websocket", "polling"],
   reconnection: true,
